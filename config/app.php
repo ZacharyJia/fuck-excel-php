d586c070e803d6c974e00293df65693602971c5e
hellowuzekai <15282022@bjtu.edu.cn>
2016-08-07 16:27:52 +0800

    新增 laravel-excel 简单实现excel读取生成工作

--- config/app.php
+++ config/app.php
@@ -161,6 +161,11 @@ return [
          */
         Jenssegers\Mongodb\MongodbServiceProvider::class,
 
+        /*
+         * laravel-excel
+         */
+        Collective\Bus\BusServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
     ],
 
     /*
@@ -206,6 +211,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Mongo'     => Jenssegers\Mongodb\MongodbServiceProvider::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
 
     ],
 
