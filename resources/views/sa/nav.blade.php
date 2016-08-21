141a956b37e8934b5804a5443f3c923296de1ee8
ZacharyJia <jia199474@gmail.com>
2016-08-21 22:18:02 +0800

    FIX 找不到user变量

--- resources/views/sa/nav.blade.php
+++ resources/views/sa/nav.blade.php
@@ -19,7 +19,7 @@
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ $user['username'] }}<span class="caret"></span></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ $login_user['username'] }}<span class="caret"></span></a>
                     <ul class="dropdown-menu">
                         <li><a href="/logout">退出</a></li>
                     </ul>
